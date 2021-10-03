2e77ae7380fa893bc84741c559049d217a7b22a9
marteyp89 <64192351+marteyp89@users.noreply.github.com>
2021-10-03 20:38:12 +0100

    Update interface.js

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,8 +1,8 @@
 
-// var minersAddr = '0x825067c910029b780e30682882a95bb77932D68F';
+// var minersAddr = '0x89A533e6490061a827E4D6908A916CD2ed57251a';
 // var tokenAddr = '0x78867bbeef44f2326bf8ddd1941a4439382ef2a7';
 
-var minersAddr = '0x825067c910029b780e30682882a95bb77932D68F';
+var minersAddr = '0x89A533e6490061a827E4D6908A916CD2ed57251a';
 var tokenAddr = '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7';
 var minersAbi =
 [
